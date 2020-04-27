920544f42e25d4d68623dc69231dcad0d8720239
shoaeb4042 <shoaeb.cse4042@gmail.com>
2020-04-27 14:19:34 +0600

    first commit

---
+++ Admin/footer.php
@@ -0,0 +1,23 @@
+<div class="footer">
+     	<div class="wrap">
+     	  <div class="footer-menu">
+     		<ul>
+				<li class="active"><a href="index.php">Home</a></li> 
+				<div class="clear"></div>
+			</ul>
+     	  </div>
+     	  <div class="footer-bottom">
+     	  	<div class="copy">
+			   <p>© 2016 Designed by <a href="http://shoaeb.com" target="_blank">RTMS TEAM</a></p>
+		    </div>
+		    <div class="social">	
+			   <ul>	
+					
+			   </ul>
+		    </div>
+		    <div class="clear"></div>
+     	  </div>
+       </div>
+     </div>
+</body>
+</html>
---
+++ footer.php
@@ -0,0 +1,25 @@
+<div class="footer">
+     	<div class="wrap">
+     	  <div class="footer-menu">
+     		<ul>
+				<li class="active"><a href="index.php">Home</a></li> 
+				<li><a href="http://shoaeb.com/">About RTMS</a></li> 
+				
+				<li><a href="http://shoaeb.com/">Privacy policy</a></li>
+		
+				<div class="clear"></div>
+			</ul>
+     	  </div>
+     	  <div class="footer-bottom">
+     	  	<div class="copy">
+			   <p>© 2016 Designed by <a href="http://shoaeb.com" target="_blank">RTMS TEAM</a></p>
+		    </div>
+		    <div class="social">	
+			 
+		    </div>
+		    <div class="clear"></div>
+     	  </div>
+       </div>
+     </div>
+</body>
+</html>
